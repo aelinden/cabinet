3cb61cfc68ee20c58e2e87a2fc9b3548d63555b4
Aidan Follestad <drummer.aidan@gmail.com>
2015-01-30 22:41:37 -0600

    Updated the action bar, file, and empty image icons.

--- app/src/main/java/com/afollestad/cabinet/fragments/NavigationDrawerFragment.java
+++ app/src/main/java/com/afollestad/cabinet/fragments/NavigationDrawerFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -16,7 +17,6 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.afollestad.cabinet.R;
 import com.afollestad.cabinet.adapters.NavigationDrawerAdapter;
@@ -30,15 +30,13 @@ public class NavigationDrawerFragment extends Fragment {
 
     private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
     private static final String STATE_TITLE = "title";
-    private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learn";
 
     private DrawerLayout mDrawerLayout;
     private RecyclerView mRecyclerView;
     private NavigationDrawerAdapter mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     private int mCurrentSelectedPosition = 1;
-    private boolean mFromSavedInstanceState;
-    private boolean mUserLearnedDrawer;
     private CharSequence mTitle;
 
     private StorageHelper mStorageHelper;
@@ -50,11 +48,9 @@ public class NavigationDrawerFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        mUserLearnedDrawer = sp.getBoolean(PREF_USER_LEARNED_DRAWER, false);
         if (savedInstanceState != null) {
             mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
             mTitle = savedInstanceState.getCharSequence(STATE_TITLE);
-            mFromSavedInstanceState = true;
         }
         mStorageHelper = new StorageHelper(getActivity(), new StorageHelper.StateListener() {
             @Override
@@ -117,60 +113,21 @@ public class NavigationDrawerFragment extends Fragment {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeButtonEnabled(true);
 
-        actionBarToolbar.setNavigationIcon(R.drawable.ic_menu_white);
-        actionBarToolbar.setNavigationOnClickListener(new View
-                .OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mDrawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
-
-        mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, actionBarToolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             @Override
             public void onDrawerSlide(View drawerView, float slideOffset) {
-
+                // Don't call super method to disable the rotating nav icon
             }
-
+        };
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+        mDrawerLayout.post(new Runnable() {
             @Override
-            public void onDrawerOpened(View drawerView) {
-                if (!isAdded()) {
-                    return;
-                }
-
-                mTitle = getActivity().getTitle();
-
-                if (!mUserLearnedDrawer) {
-                    // The user manually opened the drawer; store this flag to prevent auto-showing
-                    // the navigation drawer automatically in the future.
-                    Toast.makeText(getActivity(), R.string.drawer_longpress_hint, Toast.LENGTH_LONG).show();
-                    mUserLearnedDrawer = true;
-                    SharedPreferences sp = PreferenceManager
-                            .getDefaultSharedPreferences(getActivity());
-                    sp.edit().putBoolean(PREF_USER_LEARNED_DRAWER, true).apply();
-                }
-            }
-
-
-            public void onDrawerClosed(View drawerView) {
-                if (!isAdded()) {
-                    return;
-                }
-                getActivity().setTitle(mTitle);
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-
+            public void run() {
+                mDrawerToggle.syncState();
             }
         });
-
-        // If the user hasn't 'learned' about the drawer, open it to introduce them to the drawer,
-        // per the navigation drawer design guidelines.
-        if (!mUserLearnedDrawer && !mFromSavedInstanceState) {
-            mDrawerLayout.openDrawer(Gravity.START);
-        }
-
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     private void selectItem(int position) {
